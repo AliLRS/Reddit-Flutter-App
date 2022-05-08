deb2ada21a8ecf172f76b0a6b4622380a817d553
MoAminSafari <ma.safari138112@gmail.com>
2022-05-08 22:30:46 +0430

    Adding files

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reddit',
+      theme: ThemeData(
+        primarySwatch: Colors.orange[900],
+      ),
+      home: const HomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key, String title}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
