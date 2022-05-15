4de125faeaccc2df4a1155f14ccffd0c8067a055
MoAminSafari <ma.safari138112@gmail.com>
2022-05-15 15:43:50 +0430

    buliding login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,10 +29,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Reddit'),
-      ),
-    );
+    return loginPage();
   }
 }
