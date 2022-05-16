c5e543a77776fc4020ad382ad7570932c25a6fae
MoAminSafari <ma.safari138112@gmail.com>
2022-05-16 11:42:44 +0430

    design the page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -8,10 +8,70 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController;
+  TextEditingController _passwordController;
+  bool _passwordVisibility = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('loginPage'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log In'),
+        actions: [
+          TextButton(
+            child: Text('Sign Up'),
+            style: TextButton.styleFrom(
+              primary: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '/signup');
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                IconButton(
+                  icon: (_passwordVisibility
+                      ? Icon(Icons.visibility)
+                      : Icon(Icons.visibility_off)),
+                  onPressed: () {
+                    setState(() {
+                      _passwordVisibility = !_passwordVisibility;
+                    });
+                  },
+                ),
+              ],
+            ),
+            OutlinedButton(
+              child: Text('Log In'),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
