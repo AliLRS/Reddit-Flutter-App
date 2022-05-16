64cdc3b2a09381b9bab75032ee550cea86c2b9fe
MoAminSafari <ma.safari138112@gmail.com>
2022-05-16 10:46:49 +0430

    change loginPage to LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return loginPage();
+    return LoginPage();
   }
 }
