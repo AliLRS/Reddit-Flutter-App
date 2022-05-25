3559a237c8b432627ad3c6a34ec6fa9b923e6789
AliLRS <sorush.lamei@Gamil.com>
2022-05-25 22:36:06 +0430

    change BottomBar and add Drawer to feed page

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({Key key}) : super(key: key);
@@ -8,50 +9,79 @@ class FeedPage extends StatefulWidget {
 }
 
 class _FeedPageState extends State<FeedPage> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 1;
+  int _drawerSelectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Reddit'),
       ),
-      body: ListTile(
+      body: const ListTile(
         title: Text('Feed'),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedFontSize: 13,
-        selectedIconTheme: IconThemeData(color: Colors.deepOrange, size: 25),
-        selectedItemColor: Colors.deepOrange,
-        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold),
-        currentIndex: _selectedIndex,
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepOrange
+              ),
+              child: UserAccountsDrawerHeader(
+
+                accountName: Text('User'),
+                accountEmail: Text('User@email.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text('U'),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _drawerSelectedIndex = 0;
+                });
+              },
+            ),
+            ListTile(
+              title: Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _drawerSelectedIndex = 1;
+                });
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _drawerSelectedIndex = 2;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: ConvexAppBar(
+        color: Colors.white,
+        style: TabStyle.reactCircle,
+        backgroundColor: Colors.deepOrange,
+        items: const [
+          TabItem(icon: Icons.add, title: "Add"),
+          TabItem(icon: Icons.home_filled, title: "Home"),
+          TabItem(icon: Icons.person_search_rounded, title: "Communities"),
+        ],
+        initialActiveIndex: _selectedIndex,
         onTap: (int index) {
           setState(() {
             _selectedIndex = index;
           });
         },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_filled),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_search_rounded),
-            label: 'Communities',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'Add',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
       ),
     );
   }
