912a52525a89de502928b70f583fbd79ba0a9f3b
AliLRS <sorush.lamei@Gamil.com>
2022-05-28 00:30:20 +0430

    design profile_page

--- lib/Setting/profile_page.dart
+++ lib/Setting/profile_page.dart
@@ -12,10 +12,130 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Reddit'),
+        title: const Text('Profile'),
       ),
-      body: const ListTile(
-        title: Text('Profile page'),
+      body: ListView(
+        physics: const BouncingScrollPhysics(),
+        children: [
+          Container(
+            width: double.infinity,
+            height: 140,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+              alignment: const Alignment(0.0, 0.0),
+              child: const CircleAvatar(
+                backgroundColor: Colors.deepOrange,
+                child: Text('U',
+                    style: TextStyle(fontSize: 35, )),
+                radius: 50.0,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          Column(
+            children: const [
+              Center(
+                child: Text(
+                  "Username",
+                  style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.blueGrey,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "Username@email.com",
+                  style: TextStyle(
+                      fontSize: 25.0,
+                      color: Colors.blueGrey,
+                      letterSpacing: 2.0,
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Card(
+            margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "Posts",
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "15",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w300),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "Followers",
+                          style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Text(
+                          "200",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22.0,
+                              fontWeight: FontWeight.w300),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 50,
+          child: ElevatedButton(
+            child: const Text('Edit Profile'),
+            onPressed: () {
+              setState(
+                () {
+
+                },
+              );
+            },
+          ),
+        ),
       ),
     );
   }
