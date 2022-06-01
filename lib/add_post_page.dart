0e6d4f9aae6b30bd0f64686c7e57eec45a81ecc5
MoAminSafari <ma.safari138112@gmail.com>
2022-06-01 22:00:13 +0430

    adding @override

--- lib/add_post_page.dart
+++ lib/add_post_page.dart
@@ -18,6 +18,7 @@ class _AddPageState extends State<AddPage> {
     _bodyController = TextEditingController();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
