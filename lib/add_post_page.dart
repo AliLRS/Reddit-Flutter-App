f6a589d6e8a420734410e66184defc70e10751db
MoAminSafari <ma.safari138112@gmail.com>
2022-06-01 21:48:03 +0430

    building add post page

--- lib/add_post_page.dart
+++ lib/add_post_page.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'widgets.dart';
+import 'feed_page.dart';
+import 'package:flutter/material.dart';
 
 class AddPage extends StatefulWidget {
   const AddPage({Key key}) : super(key: key);
@@ -9,17 +10,68 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
+  TextEditingController _titleController, _bodyController;
   @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController();
+    _bodyController = TextEditingController();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Reddit'),
       ),
-      body: const ListTile(
-        title: Text('Add post'),
+      body: Container(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              style: const TextStyle(fontSize: 22),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                hintText: 'Add a title',
+              ),
+              controller: _titleController,
+              keyboardType: TextInputType.text,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    hintText: 'Add Body text'),
+                controller: _bodyController,
+                keyboardType: TextInputType.text,
+                maxLines: 10,
+              ),
+            ),
+          ],
+        ),
       ),
       drawer: const PageDrawer(),
-      bottomNavigationBar: const PageAppBar(),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          height: 50,
+          child: ElevatedButton(
+              child: const Text('Continue'),
+              onPressed: () {
+                setState(() {
+                  FeedPage.selectedIndex = 1;
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const FeedPage()));
+                });
+              }),
+        ),
+      ),
     );
   }
 }
