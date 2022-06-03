e868bf57026409f5a875a04e71b395f07e5282cb
MoAminSafari <ma.safari138112@gmail.com>
2022-06-03 09:50:50 +0430

    add community selection

--- lib/add_post_page.dart
+++ lib/add_post_page.dart
@@ -11,64 +11,92 @@ class AddPage extends StatefulWidget {
 
 class _AddPageState extends State<AddPage> {
   TextEditingController _titleController, _bodyController;
+  List<String> _communities;
+  String _communitySelected;
   @override
   void initState() {
     super.initState();
     _titleController = TextEditingController();
     _bodyController = TextEditingController();
+    _communities = [
+      'Dart',
+      'Flutter',
+      'React',
+      'Vue',
+      'Angular',
+      'Backbone',
+      'Ember',
+      'Meteor'
+    ];
+    _communitySelected = _communities[0];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Reddit'),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(
-              style: const TextStyle(fontSize: 22),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                hintText: 'Add a title',
-              ),
-              controller: _titleController,
-              keyboardType: TextInputType.text,
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 10),
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    hintText: 'Add Body text'),
-                controller: _bodyController,
-                keyboardType: TextInputType.multiline,
-                maxLines: 10,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 10),
-              height: 50,
-              width: 150,
-              child: ElevatedButton(
-                onPressed: () {
-
-                },
-                child: const Text('Add Post'),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Reddit'),
         ),
-      ),
-      drawer: const PageDrawer(),
-      bottomNavigationBar: const PageAppBar()
-    );
+        body: Container(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                style: const TextStyle(fontSize: 22),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  hintText: 'Add a title',
+                ),
+                controller: _titleController,
+                keyboardType: TextInputType.text,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      hintText: 'Add Body text'),
+                  controller: _bodyController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 10,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: DropdownButton<String>(
+                  value: _communitySelected,
+                  items: _communities.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  isExpanded: true,
+                  onChanged: (String value) {
+                    setState(() {
+                      _communitySelected = value;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                height: 50,
+                width: 150,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Add Post'),
+                ),
+              ),
+            ],
+          ),
+        ),
+        drawer: const PageDrawer(),
+        bottomNavigationBar: const PageAppBar());
   }
 }
